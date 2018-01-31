d67e4010869726e8bae2af46077248b53257d171
Pranav A <pranav.avva@gmail.com>
2018-01-31 16:59:03 +0000

    making a doge cv op mode. currently @Disabled

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/DogeCVOpMode.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import org.firstinspires.ftc.teamcode.*;
+import org.firstinspires.ftc.teamcode.hardware.bots.*;
+import org.firstinspires.ftc.teamcode.lib.auto.*;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
+@Autonomous(name="DogeCVOpMode", group="OpenFTCAutoOpMode")
+@Disabled
+public class DogeCVOpMode extends OpenFTCAutoOpMode {
+}
