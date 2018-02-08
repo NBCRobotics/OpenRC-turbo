4ce7962e8969c92eea8a29b894e9497f760fe19a
Pranav A <pranav.avva@gmail.com>
2018-02-08 14:58:24 +0000

    javadociig

--- javadoc/javadoc.sh
+++ javadoc/javadoc.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if [! $PWD = "/projects/OpenRC-turbo/javadoc"]
+then
+    echo "cd into the javadoc folder"
+    exit 1
+fi
+
 files=$(find .. -name *.java | tr '\n' ' ')
 
 echo "              FOUND THESE FILES               "
